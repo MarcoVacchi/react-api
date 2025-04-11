95d1a814d84fa1f477c4db0fbb0223b5fb61e4e4
MarcoVacchi <mvacchi816@gmail.com>
2025-04-11 14:11:24 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+
+const endPoint = ' https://www.freetestapi.com/api/v1/actresses';
+
+function App() {
+
+  const [actor, setActor] = useState([]);
+
+
+  function fetchTodos() {
+
+    axios.get(endPoint)
+      .then(result => {
+        console.log(result.data)
+        setActor(result.data)
+      });
+  };
+  useEffect(fetchTodos, []);
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
+
+
+// MILESTONE 1
+// Al caricamento dell'applicazione, recuperiamo la lista degli attori (o attrici) dalle API e stampiamoli in console.
+
+// MILESTONE 2
+// Prepariamo una card per ciascun attore/attrice, mostrandone le seguenti informazioni:
+// - nome
+// - anno nascita
+// - nazionalità
+// - biografia
+// - immagine
+// - riconoscimenti
+// - immagine
