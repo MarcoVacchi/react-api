af1949e4d9ec0971f9f849abaca74e1a9bbb6420
MarcoVacchi <mvacchi816@gmail.com>
2025-04-11 17:37:01 +0200

    feat: complete bonus 3 in all section

--- src/App.jsx
+++ src/App.jsx
@@ -10,24 +10,17 @@ function App() {
   const [selectedCategory, setSelectedCategory] = useState('');
   const [searchedName, setSearchedName] = useState('');
 
-  function searchName() {
-    if (actresses === '') {
-      alert('Insert a name!')
-    }
-  }
-
+  // Fetch functions
   function fetchActresses() {
-    axios.get(endPointActresses)
-      .then(result => {
-        setActresses(result.data);
-      });
+    axios.get(endPointActresses).then(result => {
+      setActresses(result.data);
+    });
   }
 
   function fetchActors() {
-    axios.get(endPointActors)
-      .then(result => {
-        setActors(result.data);
-      });
+    axios.get(endPointActors).then(result => {
+      setActors(result.data);
+    });
   }
 
   useEffect(() => {
@@ -44,35 +37,52 @@ function App() {
   useEffect(fetchActresses, []);
   useEffect(fetchActors, []);
 
+  // Filters
+  const filteredActresses = actresses.filter(result =>
+    result.name.toLowerCase().includes(searchedName.toLowerCase())
+  );
+
+  const filteredActors = actors.filter(result =>
+    result.name.toLowerCase().includes(searchedName.toLowerCase())
+  );
+
   return (
     <>
       <div className="container d-flex justify-content-center mt-5 mb-5">
-        <input type='text' placeholder='insert a name' onChange={searchName}></input>
+        {selectedCategory === 'all' ? <input
+          type="text"
+          placeholder="Insert a name"
+          value={searchedName}
+          onChange={(e) => setSearchedName(e.target.value)}
+        /> : ''}
         <button
           onClick={() => setSelectedCategory('actresses')}
           type="button"
-          className="btn btn-secondary mx-3">
+          className="btn btn-secondary mx-3"
+        >
           Actresses
         </button>
         <button
           onClick={() => setSelectedCategory('actors')}
           type="button"
-          className="btn btn-secondary mx-3">
+          className="btn btn-secondary mx-3"
+        >
           Actors
         </button>
         <button
           onClick={() => setSelectedCategory('all')}
           type="button"
-          className="btn btn-secondary mx-3">
+          className="btn btn-secondary mx-3"
+        >
           All
         </button>
       </div>
 
       <div className="container d-flex flex-wrap mt-3 mb-3">
-        {selectedCategory === 'actresses' && actresses.map(({ name, birth_year, nationality, biography, image, awards }, index) => (
+        {selectedCategory === 'actresses' && filteredActresses.map(({ name, birth_year, nationality, biography, image, awards }, index) => (
           <div className="card container mb-5 bg-dark-subtle" key={index}>
             <div className="card-body">
-              <h1 className="text-center">CARD ACTRESSES</h1>
+              <h1 className="text-center">CARD ACTRESS</h1>
               <h2 className="card-title">Name: {name}</h2>
               <hr />
               <h3 className="card-text">Date of Birth: {birth_year}</h3>
@@ -80,7 +90,7 @@ function App() {
             <ul className="list-group list-group-flush">
               <li className="list-group-item bg-dark-subtle"><h4>Nationality: {nationality}</h4></li>
               <li className="list-group-item bg-dark-subtle"><h5>Biography: {biography}</h5></li>
-              <img src={image} className="card-img" alt="image-current" />
+              <img src={image} className="card-img" alt="actress" />
               <li className="list-group-item bg-dark-subtle">
                 <h6><strong>Awards: {awards}</strong></h6>
               </li>
@@ -88,7 +98,7 @@ function App() {
           </div>
         ))}
 
-        {selectedCategory === 'actors' && actors.map(({ id, name, birth_year, nationality, biography, image, awards }) => (
+        {selectedCategory === 'actors' && filteredActors.map(({ id, name, birth_year, nationality, biography, image, awards }) => (
           <div className="card container mb-5 bg-dark-subtle" key={id}>
             <div className="card-body">
               <h1 className="text-center">CARD ACTOR</h1>
@@ -99,7 +109,7 @@ function App() {
             <ul className="list-group list-group-flush">
               <li className="list-group-item bg-dark-subtle"><h4>Nationality: {nationality}</h4></li>
               <li className="list-group-item bg-dark-subtle"><h5>Biography: {biography}</h5></li>
-              <img src={image} className="card-img" alt="image-current" />
+              <img src={image} className="card-img" alt="actor" />
               <li className="list-group-item bg-dark-subtle">
                 <h6><strong>Awards: {awards}</strong></h6>
               </li>
@@ -109,7 +119,7 @@ function App() {
 
         {selectedCategory === 'all' && (
           <>
-            {actresses.map(({ name, birth_year, nationality, biography, image, awards }, index) => (
+            {filteredActresses.map(({ name, birth_year, nationality, biography, image, awards }, index) => (
               <div className="card container mb-5 bg-dark-subtle" key={`actress-${index}`}>
                 <div className="card-body">
                   <h1 className="text-center">CARD ACTRESS</h1>
@@ -120,14 +130,14 @@ function App() {
                 <ul className="list-group list-group-flush">
                   <li className="list-group-item bg-dark-subtle"><h4>Nationality: {nationality}</h4></li>
                   <li className="list-group-item bg-dark-subtle"><h5>Biography: {biography}</h5></li>
-                  <img src={image} className="card-img" alt="image-current" />
+                  <img src={image} className="card-img" alt="actress" />
                   <li className="list-group-item bg-dark-subtle">
                     <h6><strong>Awards: {awards}</strong></h6>
                   </li>
                 </ul>
               </div>
             ))}
-            {actors.map(({ id, name, birth_year, nationality, biography, image, awards }) => (
+            {filteredActors.map(({ id, name, birth_year, nationality, biography, image, awards }) => (
               <div className="card container mb-5 bg-dark-subtle" key={`actor-${id}`}>
                 <div className="card-body">
                   <h1 className="text-center">CARD ACTOR</h1>
@@ -138,7 +148,7 @@ function App() {
                 <ul className="list-group list-group-flush">
                   <li className="list-group-item bg-dark-subtle"><h4>Nationality: {nationality}</h4></li>
                   <li className="list-group-item bg-dark-subtle"><h5>Biography: {biography}</h5></li>
-                  <img src={image} className="card-img" alt="image-current" />
+                  <img src={image} className="card-img" alt="actor" />
                   <li className="list-group-item bg-dark-subtle">
                     <h6><strong>Awards: {awards}</strong></h6>
                   </li>
